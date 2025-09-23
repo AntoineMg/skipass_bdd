19beb8f1f9869778d93f3c8b351cd4264d1dd13b
antoinemg <antoine.mg@outlook.com>
2025-09-23 09:22:45 +0200

    fixed query

--- ScriptInsert.php
+++ ScriptInsert.php
@@ -5,7 +5,7 @@
 
     $skipass_db = new mysqli("127.0.0.1","root","","skipass_porret_morier");
 
-    $skipass_db.query("INSERT INTO 'carte' ('numCarte','dateDebutValide','dateFinValide' VALUES
+    mysqli_query( $skipass_db,"INSERT INTO 'carte' ('numCarte','dateDebutValide','dateFinValide' VALUES
         ($card_number, $valid_start_date, $valid_end_date);
     )")
 
