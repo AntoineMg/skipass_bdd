3d995f9c972ced57809de26f9ca37492f0b30a63
Loris Porret <Loris.Porret@etu.univ-savoie.fr>
2025-09-23 10:48:07 +0200

    tableau cartes

--- script_affiche.php
+++ script_affiche.php
@@ -1,17 +1,46 @@
 <?php
 
-    $skipass_db = new mysqli("127.0.0.1","root","","skipass_porret_morier");
-    
-    $query = "SELECT * FROM carte";
-    $result = $skipass_db->query($query);
-    $enreg = $result->fetch_row();
+$skipass_db = new mysqli("127.0.0.1", "root", "", "skipass_porret_morier");
 
-    do{
-        echo $enreg[0];
-        echo $enreg[1];
-        echo $enreg[2];
-        echo $enreg[3];
-        $enreg = $result->fetch_row();
-    }while($enreg!= NULL);
-
+$query = "SELECT * FROM carte";
+$result = $skipass_db->query($query);
+$enreg = $result->fetch_row();
 ?>
+<html>
+<table border="1">
+    <tr>
+        <td> Cartes enregistrees </td>
+    </tr>
+
+</html>
+<?php
+do {
+?>
+    <tr>
+        <td>
+            <?php
+            echo $enreg[0];
+            ?>
+        </td>
+        <td>
+            <?php
+            echo $enreg[1];
+            ?>
+        </td>
+        <td>
+            <?php
+            echo $enreg[2];
+            ?>
+        </td>
+
+
+        <td>
+
+            <?php
+            echo $enreg[3];
+            ?>
+        </td>
+    </tr>
+<?php
+    $enreg = $result->fetch_row();
+} while ($enreg != NULL); ?>
