88f1019b27c199fb2439e23d6df252d2d109b496
antoinemg <antoine.mg@outlook.com>
2025-09-23 09:47:28 +0200

    f32453

--- ScriptInsert.php
+++ ScriptInsert.php
@@ -4,7 +4,6 @@
     $valid_end_date = $_POST["valid_end_date"];
 
     $skipass_db = new mysqli("127.0.0.1","root","","skipass_porret_morier");
-
     
     $sql = "INSERT INTO carte (numCarte, dateDebutValide, dateFinValide)
             VALUES ('$card_number', '$valid_start_date', '$valid_end_date')";
