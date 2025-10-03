7876824dd885db9fb878b2ebc51c5979241306e2
antoinemg <antoine.mg@outlook.com>
2025-10-03 11:57:54 +0200

    dfsghjklé

--- skipass_porret_morier.sql
+++ skipass_porret_morier.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  mer. 01 oct. 2025 à 12:01
--- Version du serveur :  5.7.17
--- Version de PHP :  5.6.30
+-- Hôte : db
+-- Généré le : ven. 03 oct. 2025 à 09:57
+-- Version du serveur : 8.0.43
+-- Version de PHP : 8.2.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,48 +18,128 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `skipass_porret_morier`
+-- Base de données : `skipass_porret_morier`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `carte`
+-- Structure de la table `cards`
 --
 
-CREATE TABLE `carte` (
-  `idCarte` int(11) UNSIGNED NOT NULL,
-  `numCarte` int(11) UNSIGNED NOT NULL,
+CREATE TABLE `cards` (
+  `idCarte` int UNSIGNED NOT NULL,
+  `numCarte` int UNSIGNED NOT NULL,
   `dateDebutValide` date NOT NULL,
   `dateFinValide` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Déchargement des données de la table `carte`
+-- Déchargement des données de la table `cards`
 --
 
-INSERT INTO `carte` (`idCarte`, `numCarte`, `dateDebutValide`, `dateFinValide`) VALUES
-(1, 213, '2025-10-02', '2025-10-17');
+INSERT INTO `cards` (`idCarte`, `numCarte`, `dateDebutValide`, `dateFinValide`) VALUES
+(3, 1345, '0053-05-13', '0534-05-13'),
+(7, 134, '2025-09-04', '2025-09-18'),
+(8, 123, '2025-10-02', '2025-09-18'),
+(9, 1234, '0123-03-12', '0132-12-31');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` int UNSIGNED NOT NULL,
+  `first_name` varchar(30) DEFAULT NULL,
+  `last_name` varchar(30) DEFAULT NULL,
+  `login` varchar(30) DEFAULT NULL,
+  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `role_id` int UNSIGNED DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `first_name`, `last_name`, `login`, `password`, `role_id`) VALUES
+(18, 'ANTOINE', 'MG', 'morierga', 'c129b324aee662b04eccf68babba85851346dff9', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `roles`
+--
+
+CREATE TABLE `roles` (
+  `role_id` int UNSIGNED NOT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
+(1, 'member'),
+(2, 'administrator');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `carte`
+-- Index pour la table `cards`
 --
-ALTER TABLE `carte`
+ALTER TABLE `cards`
   ADD PRIMARY KEY (`idCarte`);
+
+--
+-- Index pour la table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Index pour la table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `carte`
+-- AUTO_INCREMENT pour la table `cards`
 --
-ALTER TABLE `carte`
-  MODIFY `idCarte` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+ALTER TABLE `cards`
+  MODIFY `idCarte` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employee_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT pour la table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `role_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
